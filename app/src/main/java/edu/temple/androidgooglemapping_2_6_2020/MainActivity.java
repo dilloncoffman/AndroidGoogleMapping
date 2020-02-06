7f36cc243937c84524fbf25f5868881a37239869
Dillon Coffman <tug40394@temple.edu>
2020-02-06 15:46:47 -0500

    Initial commit

---
+++ app/src/main/java/edu/temple/androidgooglemapping_2_6_2020/MainActivity.java
@@ -0,0 +1,14 @@
+package edu.temple.androidgooglemapping_2_6_2020;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
